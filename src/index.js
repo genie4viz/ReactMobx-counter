7ecdec14f8c0581e1d3cd85044bcb0f3f69d9fa2
selistym <selistym@protonmail.com>
2018-12-26 07:02:41 +0200

    updated and add store ability

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from "react";
 import { render } from "react-dom";
+import { Provider } from 'mobx-react';
 import DevTools from "mobx-react-devtools";
-
+import CounterStore from './stores/counter';
 import App from "./App";
 
+const counter = new CounterStore();
 
 render(
-  <div>
-    <DevTools />
-    <App />
-  </div>,
+  <Provider counter={counter}>
+    <div>
+      <DevTools />
+      <App />
+    </div>
+  </Provider>,
   document.getElementById("root")
 ); 
